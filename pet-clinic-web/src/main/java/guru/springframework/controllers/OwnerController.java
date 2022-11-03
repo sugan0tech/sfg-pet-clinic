b8bdcff654054b6b41e490cedeab06aeb818ab9a
sugan0tech <71312159+sugan0tech@users.noreply.github.com>
2022-11-03 21:38:55 +0530

    added owner controller index page closes #16

--- pet-clinic-web/src/main/java/guru/springframework/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/controllers/OwnerController.java
@@ -1,13 +1,21 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owner")
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String getOwner(){
+    public String getOwner(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 }

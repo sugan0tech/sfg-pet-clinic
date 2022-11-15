0bb31125528124ece8db450dbdbeb364ecb18f1e
sugan0tech <sugankpms@gmail.com>
2022-11-15 18:11:05 +0530

    created pet type map service closes #28

---
+++ pet-clinic-data/src/main/java/guru/springframework/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.services;
+
+import guru.springframework.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/services/map/PetTypeMapService.java
@@ -0,0 +1,37 @@
+package guru.springframework.services.map;
+
+import guru.springframework.model.PetType;
+import guru.springframework.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService{
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public PetType save(PetType obj) {
+        return null;
+    }
+}

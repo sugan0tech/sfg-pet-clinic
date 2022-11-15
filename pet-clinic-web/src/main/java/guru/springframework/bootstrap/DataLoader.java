5e149780dd8c425009e85c18f6b28280e25eef63
sugan0tech <sugankpms@gmail.com>
2022-11-15 18:17:27 +0530

    added petType in bootstrap closes #31

--- pet-clinic-web/src/main/java/guru/springframework/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/bootstrap/DataLoader.java
@@ -6,6 +6,7 @@ import guru.springframework.model.PetType;
 import guru.springframework.model.Vet;
 import guru.springframework.services.OwnerService;
 import guru.springframework.services.PetService;
+import guru.springframework.services.PetTypeService;
 import guru.springframework.services.VetService;
 import guru.springframework.services.map.OwnerServiceMap;
 import guru.springframework.services.map.PetServiceMap;
@@ -20,16 +21,26 @@ public class DataLoader implements CommandLineRunner {
     private OwnerService ownerService;
     private PetService petService;
     private VetService vetService;
+    private PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, PetService petService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, PetService petService, PetTypeService petTypeService, VetService vetService) {
         this.ownerService = ownerService;
         this.petService = petService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("sugan");
         owner1.setLastName("muru");

ef86a3338464e4943a223482a48f9a4bcf20a183
sugan0tech <sugankpms@gmail.com>
2022-11-15 16:59:32 +0530

    created speciality and added it to vets closes #26

--- pet-clinic-data/src/main/java/guru/springframework/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/model/Vet.java
@@ -1,4 +1,13 @@
 package guru.springframework.model;
 
 public class Vet extends  Person{
+    private Speciality speciality;
+
+    public Speciality getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Speciality speciality) {
+        this.speciality = speciality;
+    }
 }

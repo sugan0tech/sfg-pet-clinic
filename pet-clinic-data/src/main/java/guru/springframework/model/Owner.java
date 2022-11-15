5351f97a2e6ac19a72ada6e09f8e85549303c764
sugan0tech <sugankpms@gmail.com>
2022-11-15 17:02:33 +0530

    added contact information to owners #27

--- pet-clinic-data/src/main/java/guru/springframework/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/model/Owner.java
@@ -3,6 +3,11 @@ package guru.springframework.model;
 import java.util.Set;
 
 public class Owner extends  Person{
+    private Set<Pet> pets;
+    private String address;
+    private String city;
+    private String telephone;
+
     public Set<Pet> getPets() {
         return pets;
     }
@@ -11,5 +16,27 @@ public class Owner extends  Person{
         this.pets = pets;
     }
 
-    private Set<Pet> pets;
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }

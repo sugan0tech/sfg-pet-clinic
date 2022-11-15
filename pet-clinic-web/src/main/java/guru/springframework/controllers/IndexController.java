401fe9759bf8e5847ff397f689aeea9cc388a5e8
sugan0tech <sugankpms@gmail.com>
2022-11-15 15:57:39 +0530

    added layout for owners list page closes #20

--- pet-clinic-web/src/main/java/guru/springframework/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,14 +1,12 @@
 package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
     @RequestMapping({"", "/", "index", "index.html"})
-    public String index(Model model){
-        model.addAttribute("welcome");
+    public String index(){
         return "index";
     }
 

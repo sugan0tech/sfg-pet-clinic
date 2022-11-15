a12656ae302432228c4d4ddbba26fdc4852b476d
sugan0tech <sugankpms@gmail.com>
2022-11-15 16:47:11 +0530

    added entities for petType, Visit closes #25

---
+++ pet-clinic-data/src/main/java/guru/springframework/model/Visit.java
@@ -0,0 +1,35 @@
+package guru.springframework.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity{
+    private LocalDate date;
+    private String description;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    private Pet pet;
+
+}

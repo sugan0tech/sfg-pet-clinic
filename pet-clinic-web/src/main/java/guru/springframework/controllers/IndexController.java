4d5d5e76ad37432fdcf7dbd20ee0a72e93610def
sugan0tech <sugankpms@gmail.com>
2022-11-15 15:12:43 +0530

    added i18n support closes #24

--- pet-clinic-web/src/main/java/guru/springframework/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,12 +1,15 @@
 package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
     @RequestMapping({"", "/", "index", "index.html"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("welcome");
         return "index";
     }
+
 }

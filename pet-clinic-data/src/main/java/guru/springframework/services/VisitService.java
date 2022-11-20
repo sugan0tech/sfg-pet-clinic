18b25b16acaf8a80d504251ac0c74a8b100d9316
sugan0tech <sugankpms@gmail.com>
2022-11-20 22:59:37 +0530

    Added Visit Service on Map Closes #45

---
+++ pet-clinic-data/src/main/java/guru/springframework/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.services;
+
+import guru.springframework.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/services/map/VisitServiceMap.java
@@ -0,0 +1,39 @@
+package guru.springframework.services.map;
+
+import guru.springframework.model.Visit;
+import guru.springframework.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends  AbstractMapService<Visit> implements VisitService {
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit obj) {
+        if(obj == null || obj.getPet().getOwner() == null || obj.getPet().getId() == null || obj.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(obj);
+    }
+
+    @Override
+    public void delete(Visit obj) {
+        super.delete(obj);
+    }
+}

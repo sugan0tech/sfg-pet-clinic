9c5f8cc8b11b7eb63361b9747f90b0351b616886
sugan0tech <sugankpms@gmail.com>
2022-11-20 22:29:56 +0530

    added jpa service for Vets closes #40

---
+++ pet-clinic-data/src/main/java/guru/springframework/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,47 @@
+package guru.springframework.services.springdatajpa;
+
+import guru.springframework.model.Vet;
+import guru.springframework.repositories.VetRepository;
+import guru.springframework.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("JPA")
+public class VetSDJpaService  implements VetService {
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).get();
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet save(Vet obj) {
+        return vetRepository.save(obj);
+    }
+
+    @Override
+    public void delete(Vet obj) {
+        vetRepository.delete(obj);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}

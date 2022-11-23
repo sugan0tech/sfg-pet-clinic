256547ba9dbc98650ec0808821da71fecd330589
sugan0tech <sugankpms@gmail.com>
2022-11-23 23:35:54 +0530

    added some changed to junit 5

---
+++ pet-clinic-web/src/test/java/guru/springframework/SfgPetClinicApplicationTest.java
@@ -0,0 +1,19 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SfgPetClinicApplicationTest {
+    @Test
+    public void contextLoads(){
+
+    }
+
+
+}

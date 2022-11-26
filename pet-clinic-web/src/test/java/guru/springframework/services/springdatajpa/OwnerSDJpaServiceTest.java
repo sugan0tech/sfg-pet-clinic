a5415867f706fbe00cbd2c9e4e977eb6f5be63f4
sugan0tech <71312159+sugan0tech@users.noreply.github.com>
2022-11-26 19:09:27 +0530

    initialised test for Owner JPA service

---
+++ pet-clinic-web/src/test/java/guru/springframework/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,37 @@
+package guru.springframework.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerSDJpaServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
